cea414a434bb4e11c96ec4056c04cac383ce94cc
muhsin <musi974798@gmail.com>
2026-02-24 11:52:14 +0530

    models created

---
+++ customer/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from core.models import User
+from seller.models import Product, ProductVariant, SellerProfile
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price_at_time = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlists")
+    wishlist_name = models.CharField(max_length=100, default="My Wishlist")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name="items")
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.IntegerField() # Use validation for 1-5
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    order_number = models.CharField(max_length=100, unique=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=20, default='PENDING')
+    order_status = models.CharField(max_length=20, default='PLACED')
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    seller = models.ForeignKey(SellerProfile, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
---
+++ seller/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from core.models import User, SubCategory
+from django.utils.text import slugify
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="seller_profile")
+    store_name = models.CharField(max_length=255)
+    store_slug = models.SlugField(unique=True)
+    gst_number = models.CharField(max_length=50)
+    pan_number = models.CharField(max_length=50)
+    bank_account_number = models.CharField(max_length=50)
+    ifsc_code = models.CharField(max_length=20)
+    business_address = models.TextField()
+    rating = models.FloatField(default=0)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Product(models.Model):
+    seller = models.ForeignKey(SellerProfile, on_delete=models.CASCADE, related_name="products")
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    model_number = models.CharField(max_length=100)
+    is_cancellable = models.BooleanField(default=True)
+    is_returnable = models.BooleanField(default=True)
+    return_days = models.IntegerField(default=7)
+    approval_status = models.CharField(max_length=20, choices=(('PENDING', 'Pending'), ('APPROVED', 'Approved'), ('REJECTED', 'Rejected')), default='PENDING')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variants")
+    sku_code = models.CharField(max_length=100, unique=True)
+    mrp = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+    weight = models.FloatField(help_text="Weight in kg")
+    length = models.FloatField(help_text="Length in cm")
+    width = models.FloatField(help_text="Width in cm")
+    height = models.FloatField(help_text="Height in cm")
+    tax_percentage = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class ProductImage(models.Model):
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, related_name="images")
+    image_url = models.URLField()
+    alt_text = models.CharField(max_length=255, blank=True)
+    is_primary = models.BooleanField(default=False)
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100) # e.g., Color, Size
+
+class AttributeOption(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE, related_name="options")
+    value = models.CharField(max_length=100) # e.g., Red, XL
+
+class VariantAttributeBridge(models.Model):
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    option = models.ForeignKey(AttributeOption, on_delete=models.CASCADE)
+
+class InventoryLog(models.Model):
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    change_amount = models.IntegerField()
+    reason = models.CharField(max_length=50)
+    performed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
